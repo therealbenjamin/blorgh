0ad9eca17638524b301fd1cf06789974777ba362
Ben Blackburn <benjamin.blackburn@metova.com>
2015-12-30 12:47:40 -0600

    adds comments to articles

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230174220) do
+ActiveRecord::Schema.define(version: 20151230184312) do
 
   create_table "blorgh_articles", force: :cascade do |t|
     t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "blorgh_comments", force: :cascade do |t|
+    t.integer  "article_id"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
